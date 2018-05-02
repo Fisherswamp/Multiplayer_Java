bc9e584ee8bc98b28a28fb3d0d608f3d57e34640
Fisherswamp <chessandpotatoes@gmail.com>
2018-05-02 01:31:21 -0400

    Added ability to add users to database, check if a user exists, and cleaned up input sanitization

--- Eclipse Workspace/Mutliplayer Gamer/src/DatabaseManager.java
+++ Eclipse Workspace/Mutliplayer Gamer/src/DatabaseManager.java
@@ -11,19 +11,30 @@ public class DatabaseManager {
 		try {
 			connection = getConnection();
 			createTable();
+			String addUser = "test";
+			while(userExists(addUser)){
+				addUser += "x";
+			}
+			addUser(addUser,"1234","5687");
+			changeProfilename(addUser,"testProfile");
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
 	
 	
-	
+	/**
+	 * Get connection to mySQL server
+	 * @return {@link Connection} object
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 */
 	private Connection getConnection() throws ClassNotFoundException, SQLException{
 		
 		String driver = "com.mysql.cj.jdbc.Driver";
 		String ip = "localhost";
 		String port  = "3306";
-		String dataBaseName = "test";
+		String dataBaseName = "test";	
 		String url = "jdbc:mysql://" + ip + ":" + port + "/" + dataBaseName;
 		String username = "testusr";
 		String password = "testpss";
@@ -35,22 +46,76 @@ public class DatabaseManager {
 		
 		return conn;
 	}
-	
+	/**
+	 * Creates database table, if it has not already been created
+	 * @throws SQLException
+	 */
 	private void createTable() throws SQLException{
 		PreparedStatement create = connection.prepareStatement(
 				"CREATE TABLE IF NOT EXISTS "
 						+ "users("
-							+ "id int NOT NULL AUTO_INCREMENT,"
 							+ "username varchar(255),"
+							+ "profilename varchar(255),"
 							+ "password varchar(255),"
 							+ "salt varchar(255),"
-							+ "start_date datetime,"
-							+ "PRIMARY KEY(id)"
+							+ "start_date datetime DEFAULT CURRENT_TIMESTAMP,"
+							+ "PRIMARY KEY(username)"
 						+ ")"
 		);
 		create.executeUpdate();
 		
 		if(Settings.debug)
 			System.out.println("Table created");
+	}
+	
+	/**
+	 * 
+	 * @param username Name to check
+	 * @return whether or not the user exists in the database
+	 * @throws SQLException
+	 */
+	private boolean userExists(String username) throws SQLException{
+		PreparedStatement check = connection.prepareStatement(
+				"SELECT * FROM users WHERE username = ?"
+		);
+		check.setString(1, username);
+		if(check.executeQuery().next()){
+			return true;
+		}
+		return false;
+	}
+	/**
+	 * 
+	 * @param username The name of the user to add
+	 * @param password The user's password (hashed)
+	 * @param salt The salt used in the hash
+	 * @throws SQLException
+	 */
+	private void addUser(String username, String password, String salt) throws SQLException{		
+		PreparedStatement add = connection.prepareStatement(
+				"INSERT INTO users(username, profilename, password,salt)"
+				+ "VALUES (?, ?, ?, ?);"
+		);
+		add.setString(1, username);
+		add.setString(2, username);
+		add.setString(3, password);
+		add.setString(4, salt);
+		add.executeUpdate();
+	}
+	/**
+	 * 
+	 * @param username username of user to change profilename
+	 * @param newProfilename new name to set
+	 * @throws SQLException
+	 */
+	private void changeProfilename(String username, String newProfilename) throws SQLException{
+		PreparedStatement change = connection.prepareStatement(
+				"UPDATE users"
+				+ " SET profilename = ?"
+				+ " WHERE username = ?;"
+		);
+		change.setString(1, newProfilename);
+		change.setString(2, username);
+		change.executeUpdate();
 	}
 }
